9b755121ec32422045f9ad86c39632fd7c48295e
selogerkkk <ruanhborges@gmail.com>
2023-08-25 10:28:54 -0300

    first commit

---
+++ conversor.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <section>
+        <main>
+            <?php
+            $inicio = date("m-d-Y", strtotime("-7 days"));
+            $fim = date("m-d-Y");
+            $url = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial=\'' . $inicio . '\'&@dataFinalCotacao=\'' . $fim . '\'&$top=1&$orderby=dataHoraCotacao%20desc&$format=json&$select=cotacaoCompra,dataHoraCotacao';
+            $dados = json_decode(file_get_contents($url), true);
+            $cotacao = $dados["value"][0]["cotacaoCompra"];
+
+            $reais = $_GET["reais"];
+            $conversao = $reais / $cotacao;
+
+            echo "<p>Seus reais valem: \$" . number_format($conversao, 2) . "</p>";
+            ?>
+            <button onclick="javascript:history.go(-1)">Voltar</button>
+        </main>
+    </section>
+</body>
+
+</html>
